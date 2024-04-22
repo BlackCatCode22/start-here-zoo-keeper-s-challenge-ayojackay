b2bb8ac81d36a26e47ccc0bd88828718c99a1752
Jacque <ayojackay@gmail.com>
2024-04-22 01:49:25 -0700

    Update Main.py
    made changes for my version

--- Main.py
+++ Main.py
@@ -4,11 +4,16 @@
 # last update 10/14/23
 # last Update 4/1/24 by dH
 # reviewed by dH, 4/8/24
+# Edited by Jacque S, 04/21/2024
 
 
 from Animal import Animal
 from Hyena import Hyena
 from Lion import Lion
+from Tiger import Tiger
+from Bear import Bear
+from Report import create_report
+
 
 from _datetime import date
 
@@ -53,9 +58,9 @@ def process_one_line(one_line):
     origin_01 = ""
     origin_02 = ""
 
-    print(one_line)
+    # print(one_line)
     groups_of_words = one_line.strip().split(",")
-    print(groups_of_words)
+    # print(groups_of_words)
     single_words = groups_of_words[0].strip().split(" ")
     age_in_years = single_words[0]
     a_sex = single_words[3]
@@ -89,38 +94,27 @@ def process_one_line(one_line):
         # add to the lion list
         list_of_lions.append(my_lion)
 
+    if "tiger" in a_species:
+        my_tiger = Tiger("aName", "anID", birth_day, color, a_sex, weight, from_zoo, current_date)
+        my_tiger.name = Tiger.get_tiger_name(my_tiger)
+        my_tiger.animal_id = "Ti" + str(Tiger.numOfTigers).zfill(2)
+        list_of_tigers.append(my_tiger)
+
+    if "bear" in a_species:
+        my_bear = Bear("aName", "anID", birth_day, color, a_sex, weight, from_zoo, current_date)
+        my_bear.name = Bear.get_bear_name(my_bear)
+        my_bear.animal_id = "Be" + str(Bear.numOfBears).zfill(2)
+        list_of_bears.append(my_bear)
+
+
 # Open arrivingAnimals.txt and read it one line at a time
 # Open the file in read mode
-file_path = r"C:\2023spring\pythonRoot\dataFiles\arrivingAnimals.txt"
+file_path = r"./arrivingAnimals.txt"
 with open(file_path, "r") as file:
     # Iterate through the file line by line
     for line in file:
         process_one_line(line)
 
-# Access the static variable numOfAnimals
-print(f"\n\nNumber of animals created: {Animal.numOfAnimals}")
 
-# Output the static variable numOfHyenas
-print(f"\n\nNumber of hyenas created: {Hyena.numOfHyenas}")
-
-# Output the static variable numOfLions
-print(f"\n\nNumber of lions created: {Lion.numOfLions}")
-
-# output the animals
-# this is zoo population
-print()
-print("Zookeeper's Challenge Zoo Population")
-print()
-print("Hyena Habitat:")
-print()
-for hyena in list_of_hyenas:
-    print(hyena.animal_id + ", " + hyena.name + "; birthdate: " + str(hyena.birth_date) + "; " + hyena.color +
-          "; " + hyena.sex + "; " + hyena.weight + "; " + hyena.originating_zoo + "; arrived: " +
-          str(hyena.date_arrival))
-print()
-print("Lion Habitat:")
-print()
-for lion in list_of_lions:
-    print(lion.animal_id + ", " + lion.name + "; birthdate: " + str(lion.birth_date) + "; " + lion.color +
-          "; " + lion.sex + "; " + lion.weight + "; " + lion.originating_zoo + "; arrived: " +
-          str(lion.date_arrival))
+animals = {"hyenas": list_of_hyenas, "lions": list_of_lions, "tigers": list_of_tigers, "bears": list_of_bears}
+create_report(animals)
